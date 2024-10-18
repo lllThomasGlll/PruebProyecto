b75cedbeeb0b15273668483cf680afe51eb9166a
Thomas Gomez <exnotgomez@ServiciosNutresa.onmicrosoft.com>
2024-10-17 23:56:20 -0500

    image animation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,42 +12,55 @@ import workSheets from "../assets/worksheets.gif";
 import geometry from "../assets/geometry.gif";
 import graphing from "../assets/graphing.gif";
 
+import worldProblemsPng from "../assets/word_problems.png";
+import solutionStepPng from "../assets/solution-steps.png";
+import calculatorsPng from "../assets/calculators.png";
+import workSheetsPng from "../assets/worksheets.png";
+import geometryPng from "../assets/geometry.png";
+import graphingPng from "../assets/graphing.png";
+
 const App = () => {
   const features = [
     {
       title: "Hojas de trabajo",
       description: "Generar hojas de trabajo para diversos temas.",
       image: calculators,
+      staticImage: calculatorsPng,
       link: "/hojas-de-trabajo",
     },
     {
       title: "Geometría",
       description: "Resolver problemas geométricos y dibujar formas.",
       image: geometry,
+      staticImage: geometryPng,
       link: "/geometria",
     },
     {
       title: "Pasos de solución",
       description: "Solucionador en un paso a paso para problemas.",
       image: graphing,
+      staticImage: graphingPng,
       link: "/pasos-de-solucion",
     },
     {
       title: "Gráficos",
       description: "Trazar funciones y analizar ecuaciones.",
       image: solutionStep,
+      staticImage: solutionStepPng,
       link: "/graficos",
     },
     {
       title: "Calculadoras",
       description: "Calculadoras para diferentes áreas.",
       image: worldProblems,
+      staticImage: worldProblemsPng,
       link: "/calculadoras",
     },
     {
       title: "Problemas de palabras",
       description: "Soluciones a problemas matemáticos.",
       image: workSheets,
+      staticImage: workSheetsPng,
       link: "/problemas-de-palabras",
     },
   ];
@@ -63,6 +76,7 @@ const App = () => {
               title={feature.title}
               description={feature.description}
               image={feature.image}
+              staticImage={feature.staticImage}
               link={feature.link}
             />
           ))}
