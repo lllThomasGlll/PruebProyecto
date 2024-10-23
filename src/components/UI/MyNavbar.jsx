90bedc9dad85e8b83b2cb63f3f98d784b133c9d2
clara <clararestreporua22@gmail.com>
2024-10-23 00:04:06 -0500

    Add useState to menu

--- src/components/UI/MyNavbar.jsx
+++ src/components/UI/MyNavbar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import "../../styles/MyNavbarStyle.css";
 
 const MyNavbar = () => {
+  const [expanded, setExpanded] = useState(false) // Estado para controlar si el menú está abierto o cerrado
+
+  const handleSelect = () => {
+    setExpanded(false); //Cerrar el menú cuando se selecciona un item 
+  }
+
   return (
-    <Navbar expand={false} className="custom-navbar">
+    <Navbar expand={false} className="custom-navbar" expanded={expanded}>
       <Container>
         <Navbar.Brand href="#home" className="navbar-brand-custom">
           Geobravo
@@ -14,19 +21,20 @@ const MyNavbar = () => {
         <Navbar.Toggle
           aria-controls="basic-navbar-nav"
           className="custom-icon"
+          onClick={() => setExpanded(expanded ? false : true)} // Alternar el menú al hacer clic
         />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/" className="nav-link-custom">
+          <Nav className="me-auto" onSelect={handleSelect}>
+            <Nav.Link as={Link} to="/" className="nav-link-custom" onClick={handleSelect}>
               GeoBravo
             </Nav.Link>
-            <Nav.Link as={Link} to="/resources" className="nav-link-custom">
+            <Nav.Link as={Link} to="/resources" className="nav-link-custom" onClick={handleSelect}>
               Recursos
             </Nav.Link>
-            <Nav.Link as={Link} to="/us" className="nav-link-custom">
+            <Nav.Link as={Link} to="/us" className="nav-link-custom" onClick={handleSelect}>
               Nosotros
             </Nav.Link>
-            <Nav.Link as={Link} to="/about" className="nav-link-custom">
+            <Nav.Link as={Link} to="/about" className="nav-link-custom" onClick={handleSelect}>
               Acerca de
             </Nav.Link>
           </Nav>
