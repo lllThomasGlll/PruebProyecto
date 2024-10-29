70d45318a15f13b0f0bd28ff2f11827b49b6297c
Thomas Gomez <exnotgomez@ServiciosNutresa.onmicrosoft.com>
2024-10-28 20:11:27 -0500

    Delete back button

--- src/components/GeoGebra/Problems.jsx
+++ src/components/GeoGebra/Problems.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import { Link } from "react-router-dom";
-import { Button } from 'react-bootstrap';
-import { FaArrowLeft } from 'react-icons/fa';
 
 const Problems = () => {
   return (
     <div>
       <h1>Problemas</h1>
-      <Link to="/">
-        <Button variant="primary" className="d-flex align-items-center">
-          <FaArrowLeft style={{ marginRight: "8px" }} /> Volver al inicio
-        </Button>
-      </Link>
     </div>
   )
 }
--- src/components/GeoGebra/Worksheets.jsx
+++ src/components/GeoGebra/Worksheets.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import { Link } from "react-router-dom";
-import { Button } from 'react-bootstrap';
-import { FaArrowLeft } from 'react-icons/fa';
-
 
 const Worksheets = () => {
   return (
     <div>
       <h1>Hojas de Trabajo </h1>
-      <Link to="/">
-        <Button variant="primary" className="d-flex align-items-center">
-          <FaArrowLeft style={{ marginRight: "8px" }} /> 
-        </Button>
-      </Link>
     </div>
   )
 }
