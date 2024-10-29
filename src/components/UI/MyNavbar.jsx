7bd575488f313a33ed4df2ac3b4621181337b853
Thomas Gomez <exnotgomez@ServiciosNutresa.onmicrosoft.com>
2024-10-28 20:12:59 -0500

    Add back button in component

--- src/components/UI/MyNavbar.jsx
+++ src/components/UI/MyNavbar.jsx
@@ -1,46 +1,86 @@
-import React from "react";
-import { useState } from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
 
 import "../../styles/MyNavbarStyle.css";
 
 const MyNavbar = () => {
-  const [expanded, setExpanded] = useState(false) // Estado para controlar si el menú está abierto o cerrado
-
+  const [expanded, setExpanded] = useState(false);
   const handleSelect = () => {
-    setExpanded(false); //Cerrar el menú cuando se selecciona un item 
-  }
+    setExpanded(false);
+  };
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const showBackButton = location.pathname !== "/";
 
   return (
-    <Navbar expand={false} className="custom-navbar" expanded={expanded}>
-      <Container>
-        <Navbar.Brand href="#home" className="navbar-brand-custom">
-          Geobravo
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="basic-navbar-nav"
-          className="custom-icon"
-          onClick={() => setExpanded(expanded ? false : true)} // Alternar el menú al hacer clic
-        />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto" onSelect={handleSelect}>
-            <Nav.Link as={Link} to="/" className="nav-link-custom" onClick={handleSelect}>
-              GeoBravo
-            </Nav.Link>
-            <Nav.Link as={Link} to="/resources" className="nav-link-custom" onClick={handleSelect}>
-              Recursos
-            </Nav.Link>
-            <Nav.Link as={Link} to="/us" className="nav-link-custom" onClick={handleSelect}>
-              Nosotros
-            </Nav.Link>
-            <Nav.Link as={Link} to="/about" className="nav-link-custom" onClick={handleSelect}>
-              Acerca de
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand={false} className="custom-navbar" expanded={expanded}>
+        <Container>
+          <Navbar.Brand
+            as={Link}
+            to="/"
+            className="navbar-brand-custom"
+            onClick={handleSelect}
+          >
+            Geobravo
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            className="custom-icon"
+            onClick={() => setExpanded(expanded ? false : true)}
+          />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto" onSelect={handleSelect}>
+              <Nav.Link
+                as={Link}
+                to="/"
+                className="nav-link-custom"
+                onClick={handleSelect}
+              >
+                GeoBravo
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/resources"
+                className="nav-link-custom"
+                onClick={handleSelect}
+              >
+                Recursos
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/us"
+                className="nav-link-custom"
+                onClick={handleSelect}
+              >
+                Nosotros
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                to="/about"
+                className="nav-link-custom"
+                onClick={handleSelect}
+              >
+                Acerca de
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Botón debajo del Navbar */}
+      {showBackButton && (
+        <Container fluid className="back-button-container">
+          <Button className="back-button" onClick={() => navigate("/")}>
+            <IoIosArrowBack size={35} />
+            Volver
+          </Button>
+        </Container>
+      )}
+    </>
   );
 };
 
