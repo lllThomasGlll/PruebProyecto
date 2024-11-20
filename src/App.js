58d436fd870e3fad20641f63d3621dd4715879de
Thomas Gomez <exnotgomez@ServiciosNutresa.onmicrosoft.com>
2024-11-19 19:51:07 -0500

    Router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { HashRouter } from "react-router-dom"; // Cambiado a HashRouter
+import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./config/router";
 import MyNavBar from "./components/UI/MyNavbar";
 import Footer from "./components/UI/Footer";
 
 const App = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div>
         <MyNavBar />
         <AppRouter />
         <Footer />
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
